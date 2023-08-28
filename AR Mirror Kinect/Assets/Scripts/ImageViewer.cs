6e3ffea620bce73548ca5b833a28add3f64b9de3
Sakti Harahap <saktiharahap2606@gmail.com>
2023-08-28 14:22:58 +0700

    Setup_Project

---
+++ AR Mirror Kinect/Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour
+{
+    public MultiSourceManager mMultiSource;
+
+    public RawImage mRawImage;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        mRawImage.texture = mMultiSource.GetColorTexture();
+    }
+}
